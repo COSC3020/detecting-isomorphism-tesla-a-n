2ac891776002ae105ebafde485d58c28b4c7fe9a
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-12 11:38:29 -0600

    test pcode

--- code.test.js
+++ code.test.js
@@ -2,3 +2,13 @@ const fs = require('fs');
 const jsc = require('jsverify');
 
 eval(fs.readFileSync('code.js')+'');
+
+//complete graph test
+//cycle graph test
+//path graph test
+//star graph test
+//helper function to duplicate graph (isomorphic copy)
+//helper function to generate a random permutation
+//function to test each of the isomorphic copies (complete, cycle, path, star) are isomorphic
+//function to test that different graphs are not isomorphic
+//js.verify for each test
