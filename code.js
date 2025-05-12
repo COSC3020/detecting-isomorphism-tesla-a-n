4d49691c8d1240a05c085bd015401e397742a859
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-12 10:21:04 -0600

    pcode

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
-are_isomorphic(graph1, graph2) {
-    return undefined;
+function are_isomorphic(graph1, graph2) {
+    //Check if they have the same # of vertices
+    //check if same # of edges
+    //group nodes by degree
+    //check if degree spreds match
+    //use a set and a map
+    //if all nodes are mapped return true
+    //try each of the nodes of same degree from graph2
+        //check if the mapping works with the existing mappings
+        //try another mapping
+    //Check if mapping node1 to node2 is compatible with existing mappings
+        // for each mapped neighbor of node1
+            // corresponding node in graph2 should be a neighbor of node2
+        //for each neighbor of node2 that has a reverse mapping
+            // corresponding node in graph1 should be a neighbor of node1
+    return true;
 }
