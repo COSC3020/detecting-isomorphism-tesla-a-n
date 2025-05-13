3c9ef48fe410fcee8624362c89d1bb68325ad78f
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-12 18:47:44 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const jsc = require('jsverify');
 
-eval(fs.readFileSync('code.js')+'');
+eval(fs.readFileSync('detecting_isomorphism.js')+'');
 
 function createCompleteGraph(n) {
     const graph = {};
