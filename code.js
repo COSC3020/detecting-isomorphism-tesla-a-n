3903b1645e215d4bef587e0ce4ef2f3529234ac1
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-12 18:48:24 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function are_isomorphic(graph1, graph2) {
-    // Quick check: same number of vertices?
+    //Check if they have the same # of vertices
     const nodes1 = Object.keys(graph1);
     const nodes2 = Object.keys(graph2);
     
@@ -7,24 +7,25 @@ function are_isomorphic(graph1, graph2) {
         return false;
     }
     
-    // Quick check: same number of edges?
+    //check if same # of edges
     const countEdges = (graph) => {
         let count = 0;
         for (const node in graph) {
-            count += graph[node].length;
+            count += Array.isArray(graph[node]) ? graph[node].length : 0;
         }
         return count / 2; // Each edge is counted twice in an adjacency list
     };
     
-    if (countEdges(graph1) !== countEdges(graph2)) {
+    // if isomorphic, theyd have the same # of edges
+     if (countEdges(graph1) !== countEdges(graph2)) {
         return false;
     }
     
-    // Group nodes by degree
+    //group nodes by degree
     const getNodesByDegree = (graph) => {
         const result = {};
         for (const node in graph) {
-            const degree = graph[node].length;
+            const degree = Array.isArray(graph[node]) ? graph[node].length : 0;
             if (!result[degree]) {
                 result[degree] = [];
             }
@@ -36,7 +37,7 @@ function are_isomorphic(graph1, graph2) {
     const nodesByDegree1 = getNodesByDegree(graph1);
     const nodesByDegree2 = getNodesByDegree(graph2);
     
-    // Check if degree distributions match
+    //check if degree spreads match
     for (const degree in nodesByDegree1) {
         if (!nodesByDegree2[degree] || 
             nodesByDegree1[degree].length !== nodesByDegree2[degree].length) {
@@ -44,46 +45,43 @@ function are_isomorphic(graph1, graph2) {
         }
     }
     
-    // Try to find an isomorphism using backtracking
+    //use a set and a map
     const used = new Set();
     const mapping = {};
-    
-    // Start the backtracking process
-    return backtrack(nodes1, 0, graph1, graph2, nodesByDegree2, used, mapping);
+
+    //backtrack to find valid mapping
+    return backtrack(0, nodes1, graph1, graph2, nodesByDegree2, used, mapping);
 }
 
-/**
- * Backtracking helper function to find a valid mapping
- */
-function backtrack(nodes1, index, graph1, graph2, nodesByDegree2, used, mapping) {
-    // Base case: all nodes are mapped
+//function backtrack helper function takes (index, nodes, graphs, nodes by degree, map)
+function backtrack(index, nodes1, graph1, graph2, nodesByDegree2, used, mapping) {
+    //if all nodes are mapped return true
     if (index === nodes1.length) {
         return true;
     }
     
     const node1 = nodes1[index];
-    const degree = graph1[node1].length;
+    const degree = Array.isArray(graph1[node1]) ? graph1[node1].length : 0;
     
-    // Try each node of the same degree from graph2
+    //try each of the nodes of same degree from graph2 to see if its already mapped
+    //for each node of nodes by degree
     for (const node2 of nodesByDegree2[degree] || []) {
         if (used.has(node2)) {
             continue; // Already mapped
         }
-        
-        // Check if this mapping works with existing mappings
+        //check if its map works with existing maps
         if (!isCompatible(node1, node2, graph1, graph2, mapping)) {
             continue;
         }
-        
-        // Try this mapping
+        //check if the mapping works with the existing mappings
+        //try another mapping and make the index node1 //backtrack needs index
         mapping[node1] = node2;
         used.add(node2);
         
-        if (backtrack(nodes1, index + 1, graph1, graph2, nodesByDegree2, used, mapping)) {
+        if (backtrack(index + 1, nodes1, graph1, graph2, nodesByDegree2, used, mapping)) {
             return true;
         }
-        
-        // Backtrack
+        //delete mapping of node1 index
         delete mapping[node1];
         used.delete(node2);
     }
@@ -91,39 +89,34 @@ function backtrack(nodes1, index, graph1, graph2, nodesByDegree2, used, mapping)
     return false;
 }
 
-/**
- * Checks if mapping node1 to node2 is compatible with existing mappings
- */
+//Check if mapping node1 to node2 is compatible with existing mappings
+//function isCompatible helper function takes (nodes, graphs, map)
 function isCompatible(node1, node2, graph1, graph2, mapping) {
-    const neighbors1 = graph1[node1];
-    const neighbors2 = graph2[node2];
+    const neighbors1 = Array.isArray(graph1[node1]) ? graph1[node1] : [];
+    const neighbors2 = Array.isArray(graph2[node2]) ? graph2[node2] : [];
     
-    // For each already mapped neighbor of node1, check if the corresponding 
-    // mapped node is a neighbor of node2
+    // for each mapped neighbor of node1
     for (const neighbor of neighbors1) {
-        if (mapping[neighbor] !== undefined) {
-            // If neighbor is mapped to mapping[neighbor], then node2 should be connected
-            // to mapping[neighbor] for this mapping to be consistent
-            if (!graph2[node2].includes(mapping[neighbor])) {
+        //if neighbor is in map
+        if (neighbor in mapping) {
+            // corresponding node in graph2 should be a neighbor of node2
+            if (!neighbors2.includes(mapping[neighbor])) {
                 return false;
             }
         }
     }
     
-    // For each neighbor of node2, check if the corresponding reverse mapping
-    // is a neighbor of node1
+    //for each neighbor of node2 that has a reverse mapping
     for (const neighbor of neighbors2) {
-        // Find if any node in graph1 is mapped to this neighbor
-        const reverseMapping = Object.entries(mapping).find(([_, val]) => val === neighbor);
+        const reverseMapping = Object.keys(mapping).find(key => mapping[key] === neighbor);
         if (reverseMapping) {
-            const [originalNode] = reverseMapping;
-            // If originalNode is mapped to neighbor, then node1 should be connected
-            // to originalNode for this mapping to be consistent
-            if (!graph1[node1].includes(originalNode)) {
+            // corresponding node in graph1 should be a neighbor of node1
+            //if the corresponding node in graph1 should be a neighbor of node1
+            if (!neighbors1.includes(reverseMapping)) {
+                //if not, return false
                 return false;
             }
         }
     }
-    
     return true;
 }
